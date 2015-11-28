7fdfad2afe3311a3ae4f3c20fb5b5f59bd292c99
Roy Ragsdale <roy@apexcno.com>
2015-11-28 13:36:28 -0500

    Renamed easylist to filterlist for clarity.

--- AdFisher/examples/adblock_simple.py
+++ AdFisher/examples/adblock_simple.py
@@ -2,9 +2,10 @@ import sys, os
 sys.path.append("/vagrant/core/web")
 import adblock_ads as ab
 
-rules = ab.AdBlockUnit().rules
+log_file = 'adblock.log.txt'
+rules = ab.AdBlockUnit(log_file=log_file).rules
 site =  site="http://www.bbc.com/news/"
-a = ab.AdBlockUnit(headless=True,easylist=rules)
+a = ab.AdBlockUnit(headless=True,filterlist=rules,log_file=log_file)
 
 a.collect_ads(site)
 a.quit()
