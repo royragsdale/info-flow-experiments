1ef006a8d9139a81df0af4e9ac91fbffbec5f2ae
Roy Ragsdale <roy@apexcno.com>
2015-12-01 20:21:54 -0500

    Added a demo of adfisher "original" for comparison

--- AdFisher/examples/demo_original_experiment.py
+++ AdFisher/examples/demo_original_experiment.py
@@ -2,18 +2,14 @@ import sys, os
 sys.path.append("../core")          # files from the core 
 import adfisher                     # adfisher wrapper function
 import web.pre_experiment.alexa     # collecting top sites from alexa
-import web.adblock_ads              # collecting ads
+import web.google_ads               # collecting ads
 
-log_file = 'adblock.log.txt'
-
-# Use a bare AdBlockUnit to fetch the filterlist and load the rules. All instances
-# will then share these rules
-adblock_rules = web.adblock_ads.AdBlockUnit(log_file=log_file).rules
+log_file = 'orig.adblock.log.txt'
 
 # Defines the browser that will be used as a "unit" and gives it a copy of the adblock_rules
 def make_browser(unit_id, treatment_id):
-    b = web.adblock_ads.AdBlockUnit(log_file=log_file, unit_id=unit_id, 
-        treatment_id=treatment_id, headless=True,rules=adblock_rules)
+    b = web.google_ads.GoogleAdsUnit(log_file=log_file, unit_id=unit_id, 
+        treatment_id=treatment_id, headless=True, browser="firefox")
     return b
 
 # Control Group treatment (blank)
@@ -26,31 +22,15 @@ def exp_treatment(unit):
 
 # Measurement - Collects ads
 # checks all the sites that adfisher could previously collect on
-# (~45 minutes)
+# (~10 minutes for src and href)
 def measurement(unit):
 
-    # from google_ads
-    sites = ["http://www.foxnews.com/us/index.html",
-            "http://www.bloomberg.com/",
-            "http://www.reuters.com/news/us",
-            "http://www.theguardian.com/us",
-            "http://timesofindia.indiatimes.com/international-home",
-            "http://www.bbc.com/news/"]
-    
+    sites = ['toi','bbc','guardian','reuters','bloomberg']
     for site in sites:
-        unit.collect_ads(site,reloads=2,delay=5)
-        #unit.visit_url(site)
-
-    #from bing_ads
-    for site in ["news", "weather", "entertainment", "sports", "money",
-                "lifestyle", "health", "foodanddrink","travel", "autos"]:
-
-        unit.collect_ads("http://www.msn.com/en-us/"+site,reloads=2,delay=5)
-        #unit.visit_url("http://www.msn.com/en-us/"+site)
+        unit.collect_ads(site=site, reloads=2, delay=5)
 
 # Shuts down the browser once we are done with it.
 def cleanup_browser(unit):
-    unit.save_data()
     unit.quit()
 
 # Blank analysis
@@ -64,6 +44,6 @@ def test_stat(observed_values, unit_assignments):
 adfisher.do_experiment(make_unit=make_browser, treatments=[control_treatment, exp_treatment], 
                         measurement=measurement, end_unit=cleanup_browser,
                         load_results=load_results, test_stat=test_stat, ml_analysis=False, 
-                        num_blocks=1, num_units=4, timeout=3000,
+                        num_blocks=1, num_units=4, timeout=2000,
                         log_file=log_file, exp_flag=True, analysis_flag=False, 
                         treatment_names=["control", "experimental"])
